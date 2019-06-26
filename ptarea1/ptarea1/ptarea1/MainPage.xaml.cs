32caf923adcc7ab05700d470700b4cb0f38299eb
Nabil Galeb <nabilgaleb@hotmail.com>
2019-06-25 20:38:09 -0400

    Intento de Tarea

---
+++ ptarea1/ptarea1/ptarea1/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Xamarin.Forms;
+
+
+namespace ptarea1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            GetProduct();
+
+        }
+
+        private async void GetProduct()
+        {
+            HttpClient client = new HttpClient();
+
+            var response = await client.GetStringAsync("https://productsdemoapp.azurewebsites.net/api/Products");
+
+            var products = JsonConvert.DeserializeObject<List<Products>>(response);
+
+            ProductsListView.ItemsSource = products;
+           
+        }
+    }
+}
---
+++ ptarea1/ptarea1/ptarea1/Products.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ptarea1
+{
+    public class Products
+    {
+        public int Id { get; set; }
+
+        public int ProductName { get; set; }
+
+        public double Price { get; set; }
+    }
+}
